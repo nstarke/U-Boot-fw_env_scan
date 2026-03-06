b500f05689080859b65e24d53e9599dc4c6eab0c
nstarke <starke.nicholas@gmail.com>
2026-03-06 16:38:08 -0600

    Updating license

--- fw_env_scan.c
+++ fw_env_scan.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0+
+// SPDX-License-Identifier: MIT License - Copyright (c) 2026 Nicholas Starke
 /*
  * Minimal U-Boot environment scanner for Linux hosts without Python.
  *
